77bcfffe84420b00e893f0407085dd6a12fc3efd
Iris Leal <iris_leal_m@hotmail.com>
2020-06-22 18:56:13 -0700

    Now have more model fields

--- tasks/models.py
+++ tasks/models.py
@@ -3,10 +3,19 @@ from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Task(models.Model):
+    PRIORITY_CHOICES = (
+        ('1', 'Urgent'),
+        ('2', 'Important'),
+        ('3', 'Low'),
+        ('4', 'Medium'),
+        ('5', 'When possible'),
+    )
     owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     title = models.CharField(max_length = 50)
     created_at = models.DateTimeField(auto_now=True)
-    event_date = models.DateTimeField(auto_now=True)
+    event_date = models.DateTimeField()
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, default='Medium')      
+    completed = models.BooleanField(default=False)
     message = models.TextField()
 
     def __str__(self):
